a94ba275c2701a2bb4d3dd210dcdafd75e59c9d3
Adam Smith <imagineadamsmith@gmail.com>
2022-09-20 07:47:54 -0700

    Added src env configs

---
+++ src/env.js
@@ -0,0 +1,18 @@
+/**
+ * ---------------------------------------------------------------------------
+ * ENVIRONMENT SETTINGS
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com>
+ */
+
+/**
+ * The protocol and full domain name for the website.
+ *
+ * @const {string}
+ */
+const SITE_URL = 'http://localhost:8080';
+
+export { SITE_URL };
+
+// vim:ts=4:et:ai:cc=79:fen:fdm=marker:eol
