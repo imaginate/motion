251a021d1dc4882e601a4ecf52a16c5034b2739f
Adam Smith <imagineadamsmith@gmail.com>
2022-09-21 08:01:23 -0700

    Added server helper isValidEmail

---
+++ srv/helpers/is-valid-email.js
@@ -0,0 +1,22 @@
+/**
+ * ---------------------------------------------------------------------------
+ * IS VALID EMAIL HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com>
+ */
+
+/** @const {!RegExp} */
+const EMAIL_PATT = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+/**
+ * @param {*} email
+ * @return {boolean}
+ */
+function isValidEmail(email) {
+    return !!email && typeof email === 'string' && EMAIL_PATT.test(email);
+}
+
+module.exports = isValidEmail;
+
+// vim:ts=4:et:ai:cc=79:fen:fdm=marker:eol
