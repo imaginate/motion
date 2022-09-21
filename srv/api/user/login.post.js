8accd33b10000a70db1b80ffdb8cd35e8d950045
Adam Smith <imagineadamsmith@gmail.com>
2022-09-21 08:02:08 -0700

    Added login api backend

---
+++ srv/api/user/login.post.js
@@ -0,0 +1,83 @@
+/**
+ * ---------------------------------------------------------------------------
+ * USER API: POST LOGIN
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ */
+
+'use strict';
+
+/** @const {!function(...string): string} */
+const { resolve } = require('path');
+/** @const {!Object} */
+const { to } = require('node-vitals')('fs');
+/** @const {!function(...string): string} */
+const { randomBytes } = require('crypto');
+/** @const {!function(*): boolean} */
+const isValidEmail = require('../../helpers/is-valid-email.js');
+
+/** @const {number} */
+const MAX_AGE = 2 ** 30;
+
+/** @const {string} */
+const USERS_FILEPATH = resolve('.data/users.json');
+/** @const {string} */
+const SESSIONS_FILEPATH = resolve('.data/sessions.json');
+
+/**
+ * @param {!Request} req
+ * @param {!Response} res
+ * @return {void}
+ */
+function postLogin(req, res) {
+
+    const loginReq = req.body;
+    const users = require(USERS_FILEPATH);
+
+    if (!isValidEmail(loginReq.email)) {
+        res.status(401);
+        res.send();
+        return;
+    }
+
+    let sessionUser = null;
+    for (const user of users) {
+        if (user.email === loginReq.email) {
+            if (user.password === loginReq.password) {
+                sessionUser = user;
+            }
+            break;
+        }
+    }
+    if (!sessionUser) {
+        res.status(401);
+        res.send();
+        return;
+    }
+
+    const sessionID = randomBytes(16).toString('hex');
+    const sessions = require(SESSIONS_FILEPATH);
+    for (let i = sessions.length - 1; i >= 0; --i) {
+        if (sessions[i].user_id === sessionUser.id) {
+            sessions.splice(i, 1);
+        }
+    }
+    sessions.push({
+        id: sessionID,
+        user_id: sessionUser.id,
+        manager: sessionUser.manager
+    });
+    to.file(JSON.stringify(sessions, null, 4), SESSIONS_FILEPATH);
+    
+    res.cookie('session', sessionID, {
+        httpOnly: true,
+        path: '/',
+        maxAge: MAX_AGE
+    });
+    res.send();
+}
+
+module.exports = postLogin;
+
+// vim:ts=4:et:ai:cc=79:fen:fdm=marker:eol
