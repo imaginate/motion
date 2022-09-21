640f2953e01e89a0218d67db4c7132aa4bc696b5
Adam Smith <imagineadamsmith@gmail.com>
2022-09-21 02:43:36 -0700

    Added login page app

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,20 @@
+/**
+ * ---------------------------------------------------------------------------
+ * LOGIN APP
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com>
+ */
+
+/** @const {!React} */
+import React from 'react';
+/** @const {!ReactDOM} */
+import ReactDOM from 'react-dom/client';
+/** @const {!function} */
+import Login from './_components/login.jsx';
+
+/** @const {!HTMLElement} */
+const main = document.getElementsByTagName('main')[0];
+/** @const {!Object} */
+const root = ReactDOM.createRoot(main);
+root.render(<Login />);
