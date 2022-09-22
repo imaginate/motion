2a17a72aa489e37844189a7eb2dd48db64ed66d9
Adam Smith <imagineadamsmith@gmail.com>
2022-09-21 17:23:28 -0700

    Added login capability to register backend

--- srv/api/user/register.post.js
+++ srv/api/user/register.post.js
@@ -12,13 +12,20 @@
 const { resolve } = require('path');
 /** @const {!Object} */
 const { to } = require('node-vitals')('fs');
+/** @const {!function} */
+const { randomBytes } = require('crypto');
 /** @const {!function(*): boolean} */
 const isValidEmail = require('../../helpers/is-valid-email.js');
 /** @const {!function(*): boolean} */
 const isValidString = require('../../helpers/is-valid-string.js');
 
+/** @const {number} */
+const MAX_AGE = 2 ** 30;
+
 /** @const {string} */
 const USERS_FILEPATH = resolve('.data/users.json');
+/** @const {string} */
+const SESSIONS_FILEPATH = resolve('.data/sessions.json');
 
 /**
  * @param {!Request} req
@@ -27,37 +34,51 @@ const USERS_FILEPATH = resolve('.data/users.json');
  */
 function postRegister(req, res) {
 
-    const regReq = req.body;
-    const users = require(USERS_FILEPATH);
-
-    if (!isValidString(regReq.first_name)
-        || !isValidString(regReq.last_name)
-        || !isValidEmail(regReq.email)
-        || !isValidString(regReq.password)
+    if (!isValidString(req.body.first_name)
+        || !isValidString(req.body.last_name)
+        || !isValidEmail(req.body.email)
+        || !isValidString(req.body.password)
     ) {
         res.status(400);
         res.send();
         return;
     }
 
+    const users = require(USERS_FILEPATH);
+
     for (const user of users) {
-        if (user.email === regReq.email) {
+        if (user.email === req.body.email) {
             res.status(409);
             res.send();
             return;
         }
     }
 
+    const userID = users.length + 1;
     users.push({
-        id: users.length + 1,
-        first_name: regReq.first_name,
-        last_name: regReq.last_name,
-        email: regReq.email,
+        id: userID,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
         manager: false,
-        password: regReq.password
+        password: req.body.password
     });
     to.file(JSON.stringify(users, null, 4), USERS_FILEPATH);
     
+    const sessionID = randomBytes(16).toString('hex');
+    const sessions = require(SESSIONS_FILEPATH);
+    sessions.push({
+        id: sessionID,
+        user_id: userID,
+        manager: false
+    });
+    to.file(JSON.stringify(sessions, null, 4), SESSIONS_FILEPATH);
+
+    res.cookie('session', sessionID, {
+        httpOnly: true,
+        path: '/',
+        maxAge: MAX_AGE
+    });
     res.send();
 }
 
