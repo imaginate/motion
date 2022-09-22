d84431c9dac71bab187c04dfc5e141e72d987c9a
Adam Smith <imagineadamsmith@gmail.com>
2022-09-22 09:11:48 -0700

    Added get users api backend

---
+++ srv/api/manager/users/<id>.get.js
@@ -0,0 +1,57 @@
+/**
+ * ---------------------------------------------------------------------------
+ * USER API: GET USERS
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ */
+
+'use strict';
+
+/** @const {!function(...string): string} */
+const { resolve } = require('path');
+/** @const {!function} */
+const authenticateManager = require('../../../helpers/authenticate-manager.js');
+
+/** @const {string} */
+const USERS_FILEPATH = resolve('.data/users.json');
+/** @const {!RegExp} */
+const ID_PATT = /^.*\/([1-9][0-9]{0,9})\/?$/;
+
+/**
+ * @param {!Request} req
+ * @param {!Response} res
+ * @return {void}
+ */
+function getUsers(req, res) {
+
+    const userID = authenticateManager(req);
+    if (!userID) {
+        res.status(401);
+        res.send();
+        return;
+    }
+    if (userID < 0) {
+        res.status(403);
+        res.send();
+        return;
+    }
+
+    const tab = ID_PATT.test(req.path)
+        ? +req.path.replace(ID_PATT, '$1')
+        : 0;
+    if (!tab || tab > 1) {
+        res.status(404);
+        res.send();
+        return;
+    }
+
+    res.set('Content-Type', 'application/json');
+    res.set('Page-Number', '1');
+    res.set('Last-Page', '1');
+    res.sendFile(USERS_FILEPATH);
+}
+
+module.exports = getUsers;
+
+// vim:ts=4:et:ai:cc=79:fen:fdm=marker:eol
