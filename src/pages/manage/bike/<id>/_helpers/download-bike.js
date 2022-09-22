c6d3ab90d7ca3a5e2dead044bcd86f45da348d62
Adam Smith <imagineadamsmith@gmail.com>
2022-09-22 04:13:27 -0700

    Added edit bike page helper downloadBike

---
+++ src/pages/manage/bike/<id>/_helpers/download-bike.js
@@ -0,0 +1,53 @@
+/**
+ * ---------------------------------------------------------------------------
+ * DOWNLOAD BIKE HELPER
+ * ---------------------------------------------------------------------------
+ * @author Adam Smith <imagineadamsmith@gmail.com> (https://github.com/imaginate)
+ * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com>
+ */
+
+/** @const {string} */
+import { SITE_URL } from '../../../../../env.js';
+
+/**
+ * This method handles the AJAX call that downloads the bike's data.
+ *
+ * @param {number} bikeID
+ * @param {!BikeDB} db
+ * @param {!function} done
+ * @return {void}
+ */
+function downloadBike(bikeID, db, done) {
+    const url = SITE_URL + '/api/user/bike/' + bikeID;
+    fetch(url)
+        .catch(err => {
+            err.message += '\nfetch("' + url + '") failed to connect with the'
+                + ' server';
+            console.error(err.message);
+            console.error(err);
+            alert('SERVER ERROR: The attempt to connect with our server to'
+                + ' download the bike data failed.');
+            setTimeout(() => { throw err }, 0);
+        })
+        .then(res => {
+            if (!res.ok) {
+                const err = new Error('fetch("' + url + '") responded with'
+                    + ' status ' + res.status);
+                console.error(err.message);
+                console.error(err);
+                alert('SERVER ERROR: The attempt to download the bike data'
+                    + ' from our server failed.');
+                setTimeout(() => { throw err }, 0);
+            }
+            return res.json();
+        })
+        .then(bike => {
+            db.bike(bike, false);
+            done();
+        })
+        .catch(err => setTimeout(() => { throw err }, 0));
+}
+
+export default downloadBike;
+
+// vim:ts=4:et:ai:cc=79:fen:fdm=marker:eol
