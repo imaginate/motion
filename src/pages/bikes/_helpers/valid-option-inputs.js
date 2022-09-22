eeae6589f1062b9e3a627eb498758dc9690b6801
Adam Smith <imagineadamsmith@gmail.com>
2022-09-22 04:10:06 -0700

    Updated bike and bikes with isValidInput helpers

--- src/pages/bikes/_helpers/valid-option-inputs.js
+++ src/pages/bikes/_helpers/valid-option-inputs.js
@@ -6,6 +6,17 @@
  * @copyright 2022 Adam A Smith <imagineadamsmith@gmail.com>
  */
 
+/** @const {!function} */
+import isValidDateInput from '../../../helpers/is-valid-date-input.js';
+/** @const {!function} */
+import isValidModelInput from '../../../helpers/is-valid-model-input.js';
+/** @const {!function} */
+import isValidColorInput from '../../../helpers/is-valid-color-input.js';
+/** @const {!function} */
+import isValidLocationInput from '../../../helpers/is-valid-location-input.js';
+/** @const {!function} */
+import isValidRatingInput from '../../../helpers/is-valid-rating-input.js';
+
 /**
  * @typedef {{
  *     tab: !function(string): boolean,
@@ -20,16 +31,6 @@
 
 /** @const {!RegExp} */
 const TAB = /^[0-9]+$/;
-/** @const {!RegExp} */
-const DATE = /^([0-9]{4})-([0-9]{1,2})-([0-9]{1,2})$/;
-/** @const {!RegExp} */
-const MODEL = /^[a-zA-Z0-9](?:[a-zA-Z0-9- ,']{0,30}[a-zA-Z0-9])?$/;
-/** @const {!RegExp} */
-const COLOR = /^[a-zA-Z0-9](?:[a-zA-Z0-9- ,']{0,14}[a-zA-Z0-9])?$/;
-/** @const {!RegExp} */
-const LOCATION = /^[a-zA-Z0-9](?:[a-zA-Z0-9- ,']{0,62}[a-zA-Z0-9])?$/;
-/** @const {!RegExp} */
-const RATING = /^[1-5]$/;
 
 /**
  * The valid page option keys and their value validation methods.
@@ -38,12 +39,12 @@ const RATING = /^[1-5]$/;
  */
 const VALID_OPTION_INPUTS = Object.create(null);
 VALID_OPTION_INPUTS.tab = validTabInput;
-VALID_OPTION_INPUTS.from = validDateInput;
-VALID_OPTION_INPUTS.to = validDateInput;
-VALID_OPTION_INPUTS.model = validModelInput;
-VALID_OPTION_INPUTS.color = validColorInput;
-VALID_OPTION_INPUTS.location = validLocationInput;
-VALID_OPTION_INPUTS.rating = validRatingInput;
+VALID_OPTION_INPUTS.from = isValidDateInput;
+VALID_OPTION_INPUTS.to = isValidDateInput;
+VALID_OPTION_INPUTS.model = isValidModelInput;
+VALID_OPTION_INPUTS.color = isValidColorInput;
+VALID_OPTION_INPUTS.location = isValidLocationInput;
+VALID_OPTION_INPUTS.rating = isValidRatingInput;
 
 /**
  * @param {string} input
@@ -51,72 +52,6 @@ VALID_OPTION_INPUTS.rating = validRatingInput;
  */
 function validTabInput(input) {
     return TAB.test(input);
-}
-
-/**
- * @param {string} input
- * @return {boolean}
- */
-function validDateInput(input) {
-    if (!DATE.test(input)) {
-        return false;
-    }
-    const inyear = +input.replace(DATE, '$1');
-    const inmonth = +input.replace(DATE, '$2');
-    const inday = +input.replace(DATE, '$3');
-    if (inyear < 2022
-        || inmonth < 1 || inmonth > 12
-        || inday < 1 || inday > 31
-    ) {
-        return false;
-    }
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    if (inyear === year) {
-        if (inmonth < month || inmonth > month + 3) {
-            return false;
-        }
-    } else if (month < 10 || inyear !== year + 1) {
-        return false;
-    } else if (inmonth > month - 9) {
-        return false;
-    }
-    const indate = new Date(inyear, inmonth - 1, inday);
-    return inday === indate.getDate() && (inmonth !== month || inday > day);
-}
-
-/**
- * @param {string} input
- * @return {boolean}
- */
-function validModelInput(input) {
-    return MODEL.test(input);
-}
-
-/**
- * @param {string} input
- * @return {boolean}
- */
-function validColorInput(input) {
-    return COLOR.test(input);
-}
-
-/**
- * @param {string} input
- * @return {boolean}
- */
-function validLocationInput(input) {
-    return LOCATION.test(input);
-}
-
-/**
- * @param {string} input
- * @return {boolean}
- */
-function validRatingInput(input) {
-    return RATING.test(input);
 }
 
 export default VALID_OPTION_INPUTS;
